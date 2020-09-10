95f8253c0d68c42b62a4519ad293b753c7dd0722
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-10 12:53:15 +0430

    create main navigation

---
+++ navigation/MainNavigation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+const MainNavigator = () => {
+    return (
+        <NavigationContainer>
+        </NavigationContainer>
+    );
+};
+
+export default MainNavigator;
